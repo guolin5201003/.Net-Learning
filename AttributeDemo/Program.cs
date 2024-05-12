e42d480332153a6881b3f0519d18ec80f8d7824c
Gray Guo <gguo@TOUCHPOINTMED.COM>
2024-05-12 13:52:36 +0800

    Add AtributeDemo

---
+++ AttributeDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AttributeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
