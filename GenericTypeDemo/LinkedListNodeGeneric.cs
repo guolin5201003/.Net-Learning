0f0ad28e4989789ba6283338766ee273d751f070
Gray Guo <gguo@TOUCHPOINTMED.COM>
2024-11-08 09:38:48 +0800

    noGeneric and Generic

---
+++ GenericTypeDemo/LinkedListNodeGeneric.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypeDemo
+{
+    public class LinkedListNode<T>
+    {
+        public T Value { get; private set; }
+        public LinkedListNode<T> Next { get; set; }
+        public LinkedListNode<T> Previous { get; set; }
+        public LinkedListNode(T value)
+        {
+            this.Value = value;
+        }
+    }
+
+    public class LinkedNodeList<T>:IEnumerable<T>
+    {
+        public LinkedListNode<T> First { get; private set; }
+        public LinkedListNode<T> Last { get; private set; }
+
+        public LinkedListNode<T> AddLast(T node)
+        {
+            LinkedListNode<T> newNode = new LinkedListNode<T>(node);
+            if (First == null)
+            {
+                First = newNode;
+                Last = First;
+            }
+            else
+            {
+                LinkedListNode<T> previous = Last;
+                Last.Next = newNode;
+                Last = newNode;
+                Last.Previous = previous;
+            }
+            return newNode;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            LinkedListNode<T> current = First;
+            while (current != null)
+            {
+                yield return current.Value;
+                current = current.Next;
+            }
+        }
+
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ GenericTypeDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace GenericTypeDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //NonGenericTest();
+
+            GenericTest();
+
+        }
+
+        private static void GenericTest()
+        {
+            var list = new LinkedNodeList<int>();
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddLast(3);
+
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            var list2 = new LinkedNodeList<string>();
+            list2.AddLast("string1");
+            list2.AddLast("string2");
+            list2.AddLast("string3");
+
+            foreach (string item in list2)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void NonGenericTest()
+        {
+            var list = new LinkedNodeList();
+            list.AddLast(1);
+            list.AddLast(2);
+            list.AddLast("string");
+
+            foreach (int item in list)//cast exception when enumerate string.
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
